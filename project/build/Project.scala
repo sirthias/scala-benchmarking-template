68f98ad94d408b4ed07e552c8996faf572c38a1f
Mathias <mathias@decodified.org>
2011-05-02 10:08:15 +0200

    Switch SBT dependency resolution to ModuleConfigurations

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,17 +3,30 @@ import Process._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   
-  //managed dependencies from built-in repos
-  val instrumenter =
-    "com.google.code.java-allocation-instrumenter" %
-    "java-allocation-instrumenter" % "2.0"
+  // -------------------------------------------------------------------------------------------------------------------
+  // All repositories *must* go here! See ModuleConfigurations below.
+  // -------------------------------------------------------------------------------------------------------------------
+  object Repositories {
+    lazy val SonatypeSnapshotsRepo = MavenRepository("Sonatype Snapshots", "http://oss.sonatype.org/content/repositories/snapshots")
+  }
+  
+  // -------------------------------------------------------------------------------------------------------------------
+  // ModuleConfigurations
+  // Every dependency that cannot be resolved from the built-in repositories (Maven Central and Scala Tools Releases)
+  // must be resolved from a ModuleConfiguration. This will result in a significant acceleration of the update action.
+  // Therefore, if repositories are defined, this must happen as def, not as val.
+  // -------------------------------------------------------------------------------------------------------------------
+  import Repositories._
+  val instrumenterModuleConfig = ModuleConfiguration("com.google.code.java-allocation-instrumenter", DefaultMavenRepository)
+  val caliperModuleConfig      = ModuleConfiguration("com.google.code.caliper", SonatypeSnapshotsRepo)
+  val gsonModuleConfig         = ModuleConfiguration("com.google.code.gson", SonatypeSnapshotsRepo)
 
-  //managed dependencies from external repos
-  val SonatypeSnapshotRepo =
-    MavenRepository("Sonatype OSS Repo",
-                    "http://oss.sonatype.org/content/repositories/snapshots")
-  val caliper = "com.google.code.caliper" % "caliper" % "1.0-SNAPSHOT"
+  // -------------------------------------------------------------------------------------------------------------------
+  // Dependencies
+  // -------------------------------------------------------------------------------------------------------------------
+  val instrumenter = "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0"
+  val caliper      = "com.google.code.caliper" % "caliper" % "1.0-SNAPSHOT"
 
-  override def fork =
-    forkRun("-cp" :: (runClasspath +++ buildLibraryJar).absString :: Nil)
+  // required tweak, if not present caliper will not see the right classpath and die with a ConfigurationException
+  override def fork = forkRun("-cp" :: (runClasspath +++ buildLibraryJar).absString :: Nil)
 }
