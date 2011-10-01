ff428ae5e315874a9f3222e57eb28b353e638322
Nathan Gerhart <nathan_gerhart@hotmail.com>
2011-10-01 12:59:48 -0600

    general cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object MyBuild extends Build {
   lazy val project = Project("root", file(".")) settings(
     organization := "com.example",
     name := "scala-benchmarking-template",
-    version := "0.7.5",
+    version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
         "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
@@ -15,26 +15,30 @@ object MyBuild extends Build {
     resolvers += "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
     // enable forking in run
     fork in run := true,
-//    javaOptions in run ++= Seq("-cp", "hey/ima/dushe"),
 
     // custom kludge to get caliper to see the right classpath
 
     // define the onLoad hook
     onLoad in Global <<= (onLoad in Global) ?? identity[State],
     {
-      val key = AttributeKey[Boolean]("loaded") // attribute key to prevent circular onLoad hook
+      // attribute key to prevent circular onLoad hook
+      val key = AttributeKey[Boolean]("loaded")
       val f = (s: State) => {
         val loaded: Boolean = s get key getOrElse false
         if (!loaded) {
           var cpString: String = ""
           // get the runtime classpath
           Project.evaluateTask(fullClasspath.in(Runtime), s) match {
-            case Some(Value(cp)) => cpString = cp.files.mkString(":") // make a colon-delimited string of the classpath
-            case _ => Nil // probably should handle an error here, but not sure you can ever get here with a working sbt
-          }
+	    // make a colon-delimited string of the classpath
+            case Some(Value(cp)) => cpString = cp.files.mkString(":")
+	    // probably should handle an error here, but not sure you can
+	    //  ever get here with a working sbt
+            case _ => Nil
+            }
           val extracted: Extracted = Project.extract(s)
           // return a state with loaded = true and javaOptions set correctly
-          extracted.append(Seq(javaOptions in run ++= Seq("-cp", cpString)), s.put(key, true))
+          extracted.append(Seq(javaOptions in run ++= Seq("-cp", cpString)),
+			   s.put(key, true))
         } else {
           // return the state, unmodified
           s
