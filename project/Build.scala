bed5a91d1b6ab7bf858c5e2eb0a9aa48e0752f47
Mathias <mathias@decodified.org>
2012-02-06 15:04:07 +0100

    Clean up buildfile

--- project/Build.scala
+++ project/Build.scala
@@ -1,48 +1,44 @@
 import sbt._
 import Keys._
 
-object MyBuild extends Build {
-  lazy val project = Project("root", file(".")) settings(
-    organization := "com.example",
+object Build extends Build {
+
+  lazy val project = Project("root", file(".")).settings(
+
+    // basics
     name := "scala-benchmarking-template",
+    organization := "com.example",
     version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.9.1",
+
+    // dependencies
     libraryDependencies ++= Seq(
         "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
         "com.google.code.gson" % "gson" % "1.7.1"
     ),
     resolvers += "sonatypeSnapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
+
     // enable forking in run
     fork in run := true,
 
-    // custom kludge to get caliper to see the right classpath
-
-    // define the onLoad hook
-    onLoad in Global <<= (onLoad in Global) ?? identity[State],
-    {
-      // attribute key to prevent circular onLoad hook
-      val key = AttributeKey[Boolean]("loaded")
-      val f = (s: State) => {
-        val loaded: Boolean = s get key getOrElse false
-        if (!loaded) {
-          var cpString: String = ""
-          // get the runtime classpath
-          Project.evaluateTask(fullClasspath.in(Runtime), s) match {
-	    // make a colon-delimited string of the classpath
-            case Some(Value(cp)) => cpString = cp.files.mkString(":")
-	    // probably should handle an error here, but not sure you can
-	    //  ever get here with a working sbt
-            case _ => Nil
-            }
-          val extracted: Extracted = Project.extract(s)
-          // return a state with loaded = true and javaOptions set correctly
-          extracted.append(Seq(javaOptions in run ++= Seq("-cp", cpString)),
-			   s.put(key, true))
-        } else {
-          // return the state, unmodified
-          s
+    // we need to add the runtime classpath as a "-cp" argument to the `javaOptions in run`, otherwise caliper
+    // will not see the right classpath and die with a ConfigurationException
+    // unfortunately `javaOptions` is a SettingsKey and `fullClasspath in Runtime` is a TaskKey, so we need to
+    // jump through these hoops here in order to feed the result of the latter into the former
+    onLoad in Global ~= { previous => state =>
+      previous {
+        state.get(key) match {
+          case None =>
+            // get the runtime classpath, turn into a colon-delimited string
+            val classPath = Project.runTask(fullClasspath in Runtime, state).get._2.toEither.right.get.files.mkString(":")
+            // return a state with javaOptionsPatched = true and javaOptions set correctly
+            Project.extract(state).append(Seq(javaOptions in run ++= Seq("-cp", classPath)), state.put(key, true))
+          case Some(_) => state // the javaOptions are already patched
         }
       }
-      onLoad in Global ~= (f compose _)
-    })
+    }
+
+  )
+
+  val key = AttributeKey[Boolean]("javaOptionsPatched") // attribute key to prevent circular onLoad hook
 }
